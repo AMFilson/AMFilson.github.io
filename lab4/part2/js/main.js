03a841db6c78bf5439f9dd3714a3b745b7ab55c5
AMFilson <andrew.filson13@gmail.com>
2026-04-10 08:00:23 -0400

    initial commit javascript
    js file

---
+++ lab4/part2/js/main.js
@@ -0,0 +1,12 @@
+/* 
+    Author: Andrew Filson (Adu Poku)
+    Filename: main.js
+    Date: April 10th 2026
+    Desc: The functions and variable definitions for the image gallery example
+*/
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
