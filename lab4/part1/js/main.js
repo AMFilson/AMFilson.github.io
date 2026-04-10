e0b9d5dcf2206b8ea62d11400ab18ce475677d56
AMFilson <andrew.filson13@gmail.com>
2026-04-10 14:04:48 -0400

    updated
    finished functions and added string replacements

--- lab4/part1/js/main.js
+++ lab4/part1/js/main.js
@@ -30,6 +30,7 @@ function returnRandomStoryString() {
   const randomEvent = randomValueFromArray(events)
   let storyText = `It was 94 Fahrenheit outside, so :${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`
 
+
   return storyText;
 }
 
@@ -38,13 +39,18 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString()
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name)
+    
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = `${Math.round(300/14)} Stone`;
+    const temperature = `${Math.round((94-32) * (5/9))} Celsius`;
+    newStory = newStory.replace(300, weight)
+    newStory = newStory.replace(94, temperature)
   }
 
   // TODO: replace "" with the correct expression
